fa4aa66c94f82bcb096148050a263cd6e0d670fb
Mike Mathew <m2mathew@me.com>
2018-01-22 13:57:47 -0600

    Updates jQuery to safer version

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-// http://tiyfe.herokuapp.com/collections/mike_m
+// http://tiyfe.herokuapp.com/collections/mike_m is the URL for data storage for this project
 
 $(document).ready(function () {
 
@@ -62,6 +62,5 @@ $(document).ready(function () {
 });
 
 },{}]},{},[1])
-
 
 //# sourceMappingURL=bundle.js.map
